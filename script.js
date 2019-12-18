f597793662a677cd9b5ca70cce84869908d4da8b
mrbanks <opebanky@gmail.com>
2019-12-18 17:31:22 +0100

    Desktop View only Finished

---
+++ script.js
@@ -0,0 +1,43 @@
+		function showTime(){
+		    var now = new Date();
+		    var hour = now.getHours();
+		    var mins = now.getMinutes();
+		    var secs = now.getSeconds();
+		    var day = now.getDay();
+		    // var day = 7;
+
+		    var alldays = document.getElementsByTagName("li");
+
+		    for(var i=0; i <= alldays.length; i++){
+		    	if((i+1) === day){
+		    		alldays[i].classList.add("active");
+		    	}
+		    }
+
+		    if(day == 3){
+		    }
+
+		    if(hour > 12){
+		    	hour = hour - 12;
+		    }
+
+		    if(hour == 0){
+		    	hour = 0;
+		    }
+
+		    if(hour < 10){
+		    	hour = '0' + hour;
+		    }
+
+		    if(mins < 10){
+		    	mins = '0' + mins;
+		    }
+
+		    if(secs < 10){
+		    	secs = '0' + secs;
+		    }
+
+		    document.getElementById("forclock").innerHTML = hour + ":" + mins + ":" + secs;
+		}
+
+		setInterval("showTime()", 1000);
